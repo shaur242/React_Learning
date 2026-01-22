672a84d1a4be550331dea61d2193fa1596052e4c
Shaurya Gupta <shauryag242@gmail.com>
2026-01-22 16:52:40 +0530

    more debugging

--- 08miniContext/src/Components/Login.jsx
+++ 08miniContext/src/Components/Login.jsx
@@ -2,24 +2,26 @@ import React, {useState, useContext} from 'react';
 import UserContext from '../Context/UserContext';
 
 function Login() {
-    const [Username, SetUsername] = useState('');
+    const [username, SetUsername] = useState('');
     const [password, SetPassword] = useState('');
     const {setUser} = useContext(UserContext)
 
     const handleSubmit = (e) => {
-    e.preventDefault()
-    setUser({Username,password})
+    e.preventDefault();
+    setUser({username,password})
 }
-
   return (
     <div>
       <h2>Login</h2>
-      <input type='text' placeholder='username' value={Username}
-      onChange={(e) => SetUsername(e.target.value) }/>
+      <input type='text'
+       placeholder='username'
+       value={username}
+      onChange={(e) =>
+       SetUsername(e.target.value) }/>
       {" "}
       <input type='text' placeholder='password'value={password}
       onChange={(e) => SetPassword(e.target.value) } />
-      <button onClick={handleSubmit()}>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
